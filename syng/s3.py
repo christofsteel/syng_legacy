345313804f31b21e030e133b4a68fc99f47373cc
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2022-10-12 00:18:12 +0200

    Only pull mp3 file from s3, if the type as an associated audioextension (like cdg + mp3)

--- syng/s3.py
+++ syng/s3.py
@@ -13,11 +13,21 @@ class S3DownloadThread(Thread):
 
     def run(self):
         print(f"Start Downloading {self.filename}")
-        audiofile = self.filename[:-4] + ".mp3"
-        self.client.fget_object("karaoke-files", self.filename,
-                                os.path.join(app.configuration["s3"]["cachedir"], self.filename))
-        self.client.fget_object("karaoke-files", audiofile,
-                                os.path.join(app.configuration["s3"]["cachedir"], audiofile))
+
+        # split filename into extension and name
+        name, ext = os.path.splitext(self.filename)
+        ext = ext[1:]
+
+        if "audioext" in app.configuration[ext]:
+            mp3_filename = name + "." + app.configuration[ext]["audioext"]
+            self.client.fget_object(
+                    app.s3_bucket, mp3_filename, os.path.join(app.configuration["s3"]["cachedir"], mp3_filename)
+                    )
+            print(f"Finished Downloading {mp3_filename}")
+
+        self.client.fget_object(
+                app.s3_bucket, self.filename, os.path.join(app.configuration["s3"]["cachedir"], self.filename)
+                )
         self.entry.path = os.path.join(app.configuration["s3"]["cachedir"], self.filename)
         print(f"Finished Downloading {self.filename}")
         self.entry.tag()
